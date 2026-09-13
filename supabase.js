425de8a4869a85ae113e01e1e88d0b39b8033c16
skovorodaroman-jpg <skovorodaroman@gmail.com>
2026-09-13 17:00:03 +0300

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,7 +1,6 @@
 const SUPABASE_URL = "https://uozokpguesqeiprxnijm.supabase.co";
 
-const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVvem9rcGd1ZXNxZWlwcnhuaWptIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODkyNjE0ODcsImV4cCI6MjEwNDgzNzQ4N30.xtb9KREeqG0U-WhSA_CQ5BX9f_3BffVuO7XTj18V-oM";
-
+const SUPABASE_KEY = "sb_publishable_xhT8kGREbdjnZTe2B4Fndw_N8E7yFK0";
 const supabaseClient = window.supabase.createClient(
     SUPABASE_URL,
     SUPABASE_KEY
