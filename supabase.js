3de757cf23f1ebf8ab02b3f291681cbdf6ef59bf
skovorodaroman-jpg <skovorodaroman@gmail.com>
2026-09-13 15:14:15 +0300

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,8 @@
+const SUPABASE_URL = "https://uozokpguesqeiprxnijm.supabase.co";
+
+const SUPABASE_KEY = "ВСТАВ_СЮДИ_СВІЙ_SB_PUBLISHABLE_KEY";
+
+const supabaseClient = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
