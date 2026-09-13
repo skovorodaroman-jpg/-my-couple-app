3d64b094631ba862ad0944404cc38ef1349e9a41
skovorodaroman-jpg <skovorodaroman@gmail.com>
2026-09-13 17:27:53 +0300

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,6 +1,7 @@
 const SUPABASE_URL = "https://uozokpguesqeiprxnijm.supabase.co";
 
 const SUPABASE_KEY = "sb_publishable_xhT8kGREbdjnZTe2B4Fndw_N8E7yFK0";
+
 const supabaseClient = window.supabase.createClient(
     SUPABASE_URL,
     SUPABASE_KEY
