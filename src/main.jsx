9954311de6aa8a5c7549d6e22c76e71657a71292
skovorodaroman-jpg <skovorodaroman@gmail.com>
2026-09-15 17:27:16 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -284,7 +284,7 @@ function HomePage({ profile, couple, partnerName, loveTime, setPage, moments }) 
       )}
     </div>
   );
-    
+} 
 
 function MomentsPage({ moments = [], setPage, addMoment, momentLoading, deleteMoment }) {
   const [showForm, setShowForm] = useState(false);
