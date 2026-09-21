cfbfa201c3688a14ca0afff07875f4f32685cfef
skovorodaroman-jpg <skovorodaroman@gmail.com>
2026-09-21 17:54:41 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2488,7 +2488,7 @@ function MoviesPage({ couple, session }) {
                     );
                   })()}
             </div>
-          </article>
+    
           ))}
         </div>
       )}
